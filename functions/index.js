a1c591cf27ee03a621ca71d7bc4a541ba566f100
Siva <shiva123@gmail.com>
2019-09-07 12:00:36 -0400

    add express server

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+
+app.get('/kim', (req, res) => {
+    response.send('KIM');
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.helloWorld = functions.https.onRequest(app);
