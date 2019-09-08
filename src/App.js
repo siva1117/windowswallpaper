60642c59ad93cfb9a0b9080500a1e28bebb21b8b
Siva <shiva123@gmail.com>
2019-09-08 10:43:52 -0400

    cleanup path names

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ class App extends Component {
                 <Suspense fallback={<div>Loading...</div>}>
                     <Switch>
                         <Route exact path="/" component={Home}/>
-                        <Route exact path="/" component={About}/>
-                        <Route exact path="/" component={Contact}/>
+                        <Route exact path="/about" component={About}/>
+                        <Route exact path="/contact" component={Contact}/>
                     </Switch>
                 </Suspense>
             </Router>
