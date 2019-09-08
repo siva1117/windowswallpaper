07894b8f33ef00e373e3a946a27c113a00746505
Siva <shiva123@gmail.com>
2019-09-08 09:44:16 -0400

    import materializecss

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+// Lib imports
+import React, { Component } from 'react';
+import M from 'materialize-css';
+
+// Custom imports
 import './App.css';
 
+class App extends Component {
+    componentDidMount() {
+        M.AutoInit();
+    }
 
-function App() {
-  return (
-    <div className="App">
-      Kim Wallpapers
-    </div>
-  );
+    render() {
+        return (
+            <div className="App">
+                <h1>Kim Wallpapers</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
