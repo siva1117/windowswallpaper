9ced417642a4e4c24ae3d455d32c4c686c73ca25
Siva <shiva123@gmail.com>
2019-09-08 10:42:43 -0400

    add lazyloading and suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // Lib imports
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { Component, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import M from 'materialize-css';
 
 // Custom imports
-import Home from './pages/home'
 import './App.css';
+
+// Lazy load pages
+const Home = lazy(()=> import('./pages/Home'));
+const About = lazy(()=> import('./pages/About'));
+const Contact = lazy(()=> import('./pages/Contact'));
 
 class App extends Component {
     componentDidMount() {
@@ -14,11 +18,18 @@ class App extends Component {
 
     render() {
         return (
+            <div className="App">
             <Router>
-                <div className="App">
-                    <Route exact path="/" component={Home}/>
-                </div>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/" component={About}/>
+                        <Route exact path="/" component={Contact}/>
+                    </Switch>
+                </Suspense>
             </Router>
+            </div>
+
         );
     }
 }
