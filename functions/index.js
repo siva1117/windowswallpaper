1204949180f822fd13c98074cee541c0c378c78a
Siva <shiva123@gmail.com>
2019-09-08 09:11:24 -0400

    add mock data

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,228 @@
+/*
+  @todo A simple mock server, should be replaced with live real data from firebase later
+*/
 const functions = require('firebase-functions');
 const express = require('express');
 
 const app = express();
 
+// Latest Wallpapers
+app.get('/api/v1/top', (req, res) => {
+    const data = [
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        }
+    ];
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data));
+});
 
-app.get('*', (req, res) => res.send('Hello World!'))
+// Top Wallpapers
+app.get('/api/v1/latest', (req, res) => {
+    const data = [
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        },
+        {
+            url: 'https://placeimg.com/1920/1080/nature',
+            title: 'This is our big Tagline!',
+            description: 'Here\'s our small slogan.'
+        }
+    ];
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(data));
+});
+
+// Not Found
+app.get('*', (req, res) => res.send('Resource does not exists'));
 
 exports.app = functions.https.onRequest(app);
