f4b2bef41cfb526392624898d97ad25e4b14a42f
Siva <shiva123@gmail.com>
2019-09-08 10:30:09 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Lib imports
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import M from 'materialize-css';
 
 // Custom imports
@@ -13,9 +14,11 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Home/>
-            </div>
+            <Router>
+                <div className="App">
+                    <Route exact path="/" component={Home}/>
+                </div>
+            </Router>
         );
     }
 }
