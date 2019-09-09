a3515121a20b34fc42aaedf4bcb343eda6602c4c
Siva <shiva123@gmail.com>
2019-09-09 15:29:39 -0400

    get data from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,41 @@
-import React from 'react';
+//Lib
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+//Custom Imports
 import Slider from '../components/Slider';
 import Mediagrid from '../components/Mediagrid/Mediagrid'
 
-function Home(props) {
-    return (
-        <div className="container">
-            <h1>Top Wallpapers</h1>
-            <Slider/>
-            <h2>Latest Wallpapers</h2>
-            <Mediagrid/>
-        </div>
-    );
+class Home extends Component {
+
+    //@todo Implement Redux and handle with actiosn
+    state = {
+        topWallpapers : [],
+        latestWallpapers : []
+    }
+
+    componentDidMount() {
+        //@todo  .env files to switch API's
+        Axios.get('/api/v1/latest')
+            .then((res)=>{
+                this.setState({
+                    latestWallpapers: res.data
+                });
+            })
+            .catch((err)=> console.log(err));
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                <h1>Top Wallpapers</h1>
+                <Slider/>
+                <h2>Latest Wallpapers</h2>
+                <Mediagrid mediaList={this.state.latestWallpapers} show={9}/>
+            </div>
+        );
+    }
 }
 
 export default Home;
