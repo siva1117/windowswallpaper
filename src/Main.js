2816284c8044097bc6ed5d947c10659644a30034
Siva <shiva123@gmail.com>
2019-09-11 17:37:02 -0400

    Add Redux Logger

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,22 @@
 //Lib imports
 import React, { Component } from 'react';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 
 //Custom imports
 import App from './App';
 import rootReducer from './redux/reducers/rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+let middleware = [thunk, logger];
+//@todo uncomment below before final deployment
+// if (process.env.NODE_ENV !== 'production') {
+//     middleware = [...middleware, logger]
+// }
+
+const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 class Main extends Component {
     render() {
