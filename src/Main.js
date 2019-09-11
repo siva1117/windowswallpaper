c04155b261564f85a98378795eef7413055d2472
Siva <shiva123@gmail.com>
2019-09-11 13:05:38 -0400

    create main component and connect redux store - separation of concerns

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import App from './App';
+import { createStore } from 'redux';
+
+// const store = createStore();
+
+class Main extends Component {
+    render() {
+        return <App/>;
+    }
+}
+
+export default Main;
